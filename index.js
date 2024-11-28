68c736ad802acc2ae6a6f008247274aefb2ab01f
kumardevofficial <112534262+kumardevofficial@users.noreply.github.com>
2024-11-28 18:46:38 +0530

    new update makes

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const createPost = async (req, res) => {
   const postData = req.body;
   const post = new Post(postData);
   try{
-    post.save();
+    await post.save();
     res.status(200).json({"message" : "user created successfully"});
   } catch (err) {
     res.status(500).json({"message":"internal server error", "err" : err});

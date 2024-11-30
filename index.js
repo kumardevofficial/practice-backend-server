e2d1aba07bc69611121d983d0d8a205ea3a1e97d
kumardevofficial <112534262+kumardevofficial@users.noreply.github.com>
2024-11-30 12:03:12 +0530

    adding cors link

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 import express from "express"
 import dbConnect from "./database.js"
+import cors from "cors";
+
 
 import {router as creatPost} from "./Routes/createPost.router.js"
 // const PORT = 3000;
 const app = express();
+
+app.use(
+  cors({
+    origin: "https://myblog-eight-mauve.vercel.app/", // Allow specific origin
+    methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
+    allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
+    credentials: true, // Allow cookies or authentication headers
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
